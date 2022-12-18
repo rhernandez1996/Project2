0eb0444c712b51c552a968e2c6c711c1f3cda393
rhernandez1996 <112348988+rhernandez1996@users.noreply.github.com>
2022-12-18 11:34:17 -0800

    app

---
+++ running.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:')); 
+db.once('open', function() {
+  // we're connected!
+const run = new mongoose.Schema({
+  name: String
+})
+const app = mongoose.model('App', run);
+
+const running = new app({ name: 'App is running!' });
+console.log(running.name); 
+});
